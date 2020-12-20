7a4a8eeadff82743b662ed8754ca6c44c6d90225
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-12-20 16:39:57 +0200

    Add project files.

---
+++ ND3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ND3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var items = new List<SellableItem>()
+			{
+				new SellableItem()
+				{
+					Id = 1,
+					Name = "Sellable1"
+				},
+				new SellableItem()
+				{
+					Id = 2,
+					Name = "Sellable2"
+				},
+				new SellableItem()
+				{
+					Id = 3,
+					Name = "Sellable3"
+				},
+				new SellableItem()
+				{
+					Id = 12,
+					Name = "Sellable3"
+				}
+			};
+
+			var linqIds = items.Where(item => item.Name == "Sellable3").Select(item => item.Id).ToList();
+
+			var a = 0;
+		}
+    }
+}
---
+++ ND3/SellableItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ND3
+{
+    class SellableItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
